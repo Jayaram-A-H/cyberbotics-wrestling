d198483c7b8f91cb8b2609a4f8f5ecea7a3e536f
Jayaram-A-H <1stjayaram@gmail.com>
2023-04-11 10:36:35 +0530

    farward 50

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -1,20 +1,33 @@
-from controller import Robot, Motion
+import sys
+from controller import Robot
+sys.path.append('..') # adding the utils folder to get access to some custom helper functions, have a look at it
+from utils.motion_library import MotionLibrary
 
 
-class Alice (Robot):
+class Bob (Robot):
+    def __init__(self):
+        super().__init__()
+        # to load all the motions from the motion folder, we use the Motion_library class:
+        self.library = MotionLibrary()
+
+        # we initialize the shoulder pitch motors using the Robot.getDevice() function:
+        self.RShoulderPitch = self.getDevice("RShoulderPitch")
+        self.LShoulderPitch = self.getDevice("LShoulderPitch")
+
     def run(self):
-        # motion files are text files containing pre-recorded positions of the robot's joints
-        handWave = Motion('../motions/TurnLeft20.motion')
-        handWave.setLoop(True)
-        handWave.play()
-        # retrieves the simulation time step (ms) from the world file
+        # to control a motor, we use the setPosition() function:
+        self.RShoulderPitch.setPosition(1.3)
+        self.LShoulderPitch.setPosition(1.3)
+        # for more motor control functions, see the documentation: https://cyberbotics.com/doc/reference/motor
+        # to see the list of available devices, see the NAO documentation: https://cyberbotics.com/doc/guide/nao
+
         time_step = int(self.getBasicTimeStep())
-        while self.step(time_step) != -1:  # Mandatory function to make the simulation run
-            pass
+        while self.step(time_step) != -1:
+            if self.getTime() == 1: # We wait a bit for the robot to stabilise
+                # to play a motion from the library, we use the play() function as follows:
+                self.library.play('Forwards50')
 
 
 # create the Robot instance and run main loop
-wrestler = Alice()
+wrestler = Bob()
 wrestler.run()
-
-
