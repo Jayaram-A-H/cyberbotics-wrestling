91bccd771bff55d522c676225724fa8f9ddbe9cd
Jayaram Hemachander <1stjayaram@gmail.com>
2023-04-11 09:13:34 +0530

    mov right

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -11,21 +11,16 @@ class Bob (Robot):
         self.library = MotionLibrary()
 
         # we initialize the shoulder pitch motors using the Robot.getDevice() function:
-        self.RShoulderPitch = self.getDevice("RShoulderPitch")
-        self.LShoulderPitch = self.getDevice("LShoulderPitch")
+        
 
     def run(self):
-        # to control a motor, we use the setPosition() function:
-        self.RShoulderPitch.setPosition(1.3)
-        self.LShoulderPitch.setPosition(1.3)
+        # to control a motor, we use the setPosition() funct
         # for more motor control functions, see the documentation: https://cyberbotics.com/doc/reference/motor
         # to see the list of available devices, see the NAO documentation: https://cyberbotics.com/doc/guide/nao
 
         time_step = int(self.getBasicTimeStep())
         while self.step(time_step) != -1:
-            if self.getTime() == 1: # We wait a bit for the robot to stabilise
-                # to play a motion from the library, we use the play() function as follows:
-                self.library.play('SideStepRight')
+            self.library.play('SideStepRight')
 
 
 # create the Robot instance and run main loop
