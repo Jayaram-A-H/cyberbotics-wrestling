5eceff645fe1468044da505248101a387f2ea012
Jayaram-A-H <1stjayaram@gmail.com>
2023-04-12 11:24:26 +0530

    forwards

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -47,7 +47,7 @@ class Wrestler (Robot):
         # retrieves the WorldInfo.basicTimeTime (ms) from the world file
         time_step = int(self.getBasicTimeStep())
         while self.step(time_step) != -1:  # mandatory function to make the simulation run
-            motion_library.play('SideStepLeftLoop')
+            motion_library.play('Forwards')
             #img = self.camera.get_image()
             #_,_, horizontal = IP.locate_opponent(img)
             #if horizontal is None:
