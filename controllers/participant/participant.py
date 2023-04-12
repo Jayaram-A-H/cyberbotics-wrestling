9ce1012bebfd856f6e3cb401da2e5eac04798d68
Jayaram Hemachander <1stjayaram@gmail.com>
2023-04-12 12:26:20 +0530

    forward my code

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -1,53 +1,37 @@
-import sys
 from controller import Robot
+import sys
+
+
+# We provide a set of utilities to help you with the development of your controller. You can find them in the utils folder.
+# If you want to see a list of examples that use them, you can go to https://github.com/cyberbotics/wrestling#demo-robot-controllers
 sys.path.append('..')
+from utils.motion_library import MotionLibrary
+from utils.camera import Camera
+from utils.image_processing import ImageProcessing as IP
 from utils.accelerometer import Accelerometer
 from utils.finite_state_machine import FiniteStateMachine
-from utils.motion_library import MotionLibrary
 from utils.current_motion_manager import CurrentMotionManager
 
-
-class David (Robot):
+class Wrestler (Robot): 
     def __init__(self):
-        Robot.__init__(self)
-
-        # retrieves the WorldInfo.basicTimeTime (ms) from the world file
-        self.time_step = int(self.getBasicTimeStep())
-        # the Finite State Machine (FSM) is a way of representing a robot's behavior as a sequence of states
-        self.fsm = FiniteStateMachine(
-            states=['DEFAULT', 'BLOCKING_MOTION', 'FRONT_FALL', 'BACK_FALL'],
+        Robot.__init__()
+        self.time_step = int(self.getBasicTimeStep())    
+        self.fsm=FiniteStateMachine(
+            states=['DEFAULT','FRONT_FALL','BACK_FALL','BLOCKING_MOTION'],
             initial_state='DEFAULT',
             actions={
-                'BLOCKING_MOTION': self.pending,
-                'DEFAULT': self.walk,
-                'FRONT_FALL': self.front_fall,
-                'BACK_FALL': self.back_fall
+            'DEFAULT':self.walk,
+            'BLOCKING_MOTION':self.pending,
+            'FRONT_FALL':self.front_fall,
+            'BACK_FALL':self.back_fall
             }
         )
-        self.accelerometer = Accelerometer(self, self.time_step)
-        self.leds = {
-            'right': self.getDevice('Face/Led/Right'),
-            'left': self.getDevice('Face/Led/Left')
-        }
-
-        # Shoulder roll motors for getting up from a side fall
         self.RShoulderRoll = self.getDevice('RShoulderRoll')
         self.LShoulderRoll = self.getDevice('LShoulderRoll')
-
-        # load motion files
+        self.accelerometer = Accelerometer(self, self.time_step)
         self.current_motion = CurrentMotionManager()
-        self.library = MotionLibrary()
-
-    def run(self):
-        self.leds['right'].set(0x0000ff)
-        self.leds['left'].set(0x0000ff)
-        self.current_motion.set(self.library.get('Stand'))
-        self.fsm.transition_to('BLOCKING_MOTION')
-
-        while self.step(self.time_step) != -1:
-            self.detect_fall()
-            self.fsm.execute_action()
-
+        self.motion_library = MotionLibrary()
+    
     def detect_fall(self):
         '''Detect a fall and update the FSM state.'''
         [acc_x, acc_y, _] = self.accelerometer.get_new_average()
@@ -68,8 +52,8 @@ class David (Robot):
             self.fsm.transition_to('DEFAULT')
 
     def walk(self):
-        if self.current_motion.get() != self.library.get('Forwards50'):
-            self.current_motion.set(self.library.get('Forwards50'))
+        if self.current_motion.get() != self.library.get('Forwardloop'):
+            self.current_motion.set(self.library.get('Forwardloop'))
 
     def front_fall(self):
         self.current_motion.set(self.library.get('GetUpFront'))
@@ -80,6 +64,26 @@ class David (Robot):
         self.fsm.transition_to('BLOCKING_MOTION')
 
 
-# create the Robot instance and run main loopkj
-wrestler = David()
+    def run(self):
+        # to load all the motions from the motions folder, we use the MotionLibrary class:
+        #self.camera = Camera(self)
+        
+        # retrieves the WorldInfo.basicTimeTime (ms) from the world file
+        self.current_motion.set(self.library.get('Forwards'))
+        self.fsm.transition_to('BLOCKING_MOTION')
+        while self.step(self.time_step) != -1:  # mandatory function to make the simulation run
+            self.detect_fall()
+            self.fsm.execute_action()
+            #img = self.camera.get_image()
+            #_,_, horizontal = IP.locate_opponent(img)
+            #if horizontal is None:
+            #    hor=0
+            #else: hor= horizontal * 2 / img.shape[1] - 1
+            
+            
+
+
+
+# create the Robot instance and run main loop
+wrestler = Wrestler()
 wrestler.run()
