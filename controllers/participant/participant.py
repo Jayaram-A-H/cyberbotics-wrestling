194b6e5fc82393adfd3818e4ee158dccb897ddb2
Jayaram-A-H <1stjayaram@gmail.com>
2023-04-12 15:42:41 +0530

    wake up

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -25,16 +25,72 @@ sys.path.append('..')
 from utils.motion_library import MotionLibrary
 from utils.camera import Camera
 from utils.image_processing import ImageProcessing as IP
+from utils.accelerometer import Accelerometer
+from utils.finite_state_machine import FiniteStateMachine
+from utils.current_motion_manager import CurrentMotionManager
 
-class Wrestler (Robot):     
+class Wrestler (Robot): 
+    def __init__(self):
+        super().__init__()
+        self.time_step = int(self.getBasicTimeStep())    
+        self.fsm=FiniteStateMachine(
+            states=['DEFAULT','FRONT_FALL','BACK_FALL','BLOCKING_MOTION'],
+            initial_state='DEFAULT',
+            actions={
+            'DEFAULT':self.walk,
+            'BLOCKING_MOTION':self.pending,
+            'FRONT_FALL':self.front_fall,
+            'BACK_FALL':self.back_fall
+            }
+        )
+        self.RShoulderRoll = self.getDevice('RShoulderRoll')
+        self.LShoulderRoll = self.getDevice('LShoulderRoll')
+        self.accelerometer = Accelerometer(self, self.time_step)
+        self.current_motion = CurrentMotionManager()
+        self.motion_library = MotionLibrary()
+    
+    def detect_fall(self):
+        '''Detect a fall and update the FSM state.'''
+        [acc_x, acc_y, _] = self.accelerometer.get_new_average()
+        if acc_x < -7:
+            self.fsm.transition_to('FRONT_FALL')
+        elif acc_x > 7:
+            self.fsm.transition_to('BACK_FALL')
+        if acc_y < -7:
+            # Fell to its right, pushing itself on its back
+            self.RShoulderRoll.setPosition(-1.2)
+        elif acc_y > 7:
+            # Fell to its left, pushing itself on its back
+            self.LShoulderRoll.setPosition(1.2)
+
+    def pending(self):
+        # waits for the current motion to finish before doing anything else
+        if self.current_motion.is_over():
+            self.fsm.transition_to('DEFAULT')
+
+    def walk(self):
+        if self.current_motion.get() != self.library.get('ForwardLoop'):
+            self.current_motion.set(self.library.get('ForwardLoop'))
+
+    def front_fall(self):
+        self.current_motion.set(self.library.get('GetUpFront'))
+        self.fsm.transition_to('BLOCKING_MOTION')
+
+    def back_fall(self):
+        self.current_motion.set(self.library.get('GetUpBack'))
+        self.fsm.transition_to('BLOCKING_MOTION')
+
+
     def run(self):
         # to load all the motions from the motions folder, we use the MotionLibrary class:
         #self.camera = Camera(self)
-        motion_library = MotionLibrary()
+        
         # retrieves the WorldInfo.basicTimeTime (ms) from the world file
-        time_step = int(self.getBasicTimeStep())
-        while self.step(time_step) != -1:  # mandatory function to make the simulation run
-            motion_library.play('Forwards')
+        self.current_motion.set(self.library.get('Stand'))
+        self.fsm.transition_to('BLOCKING_MOTION')
+        while self.step(self.time_step) != -1:  # mandatory function to make the simulation run
+            self.detect_fall()
+            self.fsm.execute_action()
             #img = self.camera.get_image()
             #_,_, horizontal = IP.locate_opponent(img)
             #if horizontal is None:
