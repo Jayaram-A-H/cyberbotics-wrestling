0976aaf8f4bdaa941b97174c06a64e2edac5c2bf
Jayaram Hemachander <1stjayaram@gmail.com>
2023-05-16 10:58:09 +0530

    h2

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -1,4 +1,4 @@
-# Copyright 1996-2022 Cyberbotics Ltd.
+# Copyright 1996-2023 Cyberbotics Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,46 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Controller example for the Robot Wrestling Tournament.
-Demonstrates how to access the LEDs and play a custom motion file.
-Beats Bob by moving forwards and pushing him to the ground.
-"""
+"""Minimalist controller example for the Robot Wrestling Tournament.
+   Demonstrates how to play a simple motion file."""
 
 from controller import Robot
 import sys
+
+# We provide a set of utilities to help you with the development of your controller. You can find them in the utils folder.
+# If you want to see a list of examples that use them, you can go to https://github.com/cyberbotics/wrestling#demo-robot-controllers
 sys.path.append('..')
 from utils.motion_library import MotionLibrary
 
 
-class Charlie (Robot):
-    def __init__(self):
-        Robot.__init__(self)
-        self.time_step = int(self.getBasicTimeStep())
-
-        # there are 7 controllable LEDs on the NAO robot, but we will use only the ones in the eyes
-        self.leds = {
-            'right': self.getDevice('Face/Led/Right'),
-            'left':  self.getDevice('Face/Led/Left')
-        }
-
-        self.library = MotionLibrary()
-        # adding a custom motion to the library
-        self.library.add('Shove', './Shove.motion', loop=True)
-
+class Wrestler (Robot):
     def run(self):
-        self.library.play('Stand')
-
-        self.leds['right'].set(0xff0000)  # set the eyes to red
-        self.leds['left'].set(0xff0000)
-
-        while self.step(self.time_step) != -1:
-            # When the robot is done standing for stabilization, it moves forwards
-            if self.library.get('Stand').isOver():
-                self.library.play('ForwardLoop')  # walk forward
-                self.library.play('Shove')        # play the shove motion
+        # to load all the motions from the motions folder, we use the MotionLibrary class:
+        motion_library = MotionLibrary()
+        # retrieves the WorldInfo.basicTimeTime (ms) from the world file
+        time_step = int(self.getBasicTimeStep())
+        while self.step(time_step) != -1:  # mandatory function to make the simulation run
+            motion_library.play('Backwards')
 
 
 # create the Robot instance and run main loop
-wrestler = Charlie()
+wrestler = Wrestler()
 wrestler.run()
